89dfdd34e69f7a4ec94f212f1018a94f7ee630a7
FranNole <mezherg43@gmail.com>
2025-09-26 19:32:04 -0300

    Agrego Controler Attendance

--- src/attendance/attendance.module.ts
+++ src/attendance/attendance.module.ts
@@ -6,10 +6,11 @@ import { AttendanceService } from './attendance.service';
 import { CheckinController } from './checkin/checkin.controller';
 import { ClassSession } from '../class/class-session.entity';
 import { Reservation } from '../reservation/reservation.entity';
+import { AttendanceController } from './attendance.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Attendance, ClassSession, Reservation])],
   providers: [AttendanceService],
-  controllers: [CheckinController],
+  controllers: [CheckinController, AttendanceController],
 })
 export class AttendanceModule {}
